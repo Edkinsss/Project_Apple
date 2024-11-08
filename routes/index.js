a36d9f720239116f9135fcd3736acad715bcb3db
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2024-11-08 12:38:49 +0300

    Пункт 4_2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' }); });
+
+/*Страница macOS */
+router.get('/macOS', function(req, res, next) {
+  res.send("<h1> Cтраница macOS <h1> ")
+});
+
+/* Страница Apple_watch */
+router.get('/Applewatch', function(req, res, next) { 
+  res.send("<h1>Страница Applewatch</h1>")
+});
+
+/* Страница Iphone */
+router.get('/Iphone', function(req, res, next) {
+  res.send("<h1>Страница Iphone</h1>")
 });
 
 module.exports = router;
