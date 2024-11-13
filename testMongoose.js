72afd59270fe4e82a36c706429f3ee5ea959b9a6
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2024-11-14 02:33:23 +0300

    tc_7_2

--- testMongoose.js
+++ testMongoose.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
 
-const technology = mongoose.model('technology', { name: String });
+var schema = mongoose.Schema({ name: String })
+
+schema.methods.speaker = function(){
+    console.log(this.name + " this is incredible ")
+}
+
+const technology = mongoose.model('technology', schema);
+
+
 const technique = new technology({ name: 'apple' });
-technique.save().then(() => console.log('nice try'));
+technique.save().then(() => technique.speaker());
