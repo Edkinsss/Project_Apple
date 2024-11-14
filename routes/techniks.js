b559131a92b24068758d5f2e4741d2165b17369a
Эдем Сейтджалилов <edemseytdzhalilov@MacBook-Air-Edem.local>
2024-11-14 17:38:59 +0300

    tc_8_3

--- routes/techniks.js
+++ routes/techniks.js
@@ -1,5 +1,7 @@
 var express = require('express');
+const { Technology } = require('../models/technology');
 var router = express.Router();
+var technologies = require('../models/technology').technologies;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,6 +11,19 @@ router.get('/', function(req, res, next) {
 //  Страница после слеша /techniks/....
 router.get("/:nick", function(req, res, next) {
     res.send(req.params.nick);
+});
+
+
+router.get("/:nick", async function(req, res, next) {
+    var technologies = await technology.find({nick: req.params.nick});
+    console.log(technologies)
+    if(!technologies.length) return next(new Error("Нет такой модели, в разработке."))
+        var technology = technologies[0];
+        res.render('techniks', {
+            title: technology.title,
+            picture: technology.avatar,
+            desc: technology.desc
+        }) 
 });
 
 module.exports = router;
